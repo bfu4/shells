f048a49fbc66094885061022fccfa1bcdd2c1bd6
bfu <46942366+bfu4@users.noreply.github.com>
2020-07-12 04:13:43 -0400

    added libideviceactivation#1

--- instmob.sh
+++ instmob.sh
@@ -5,17 +5,31 @@ brew install cython
 brew install gnutls
 brew install libgcrypt
 brew install libzip
+brew install libtool
+brew install libplist
+brew install libxml2
+brew install 
 
 printf  "\033[1;33m<!>\033[1;31m dependencies installed\033[1;33m<!>\033[0m\n"
 
 cd ~/Library/Caches/Homebrew/libimobiledevice--git
 
-printf  "\033[1;33m<!>\033[1;31m INSTALLING LIBIMOBILEDEVICE USING GNUTLS\033[1;33m<!>\033[0m\n"printf "{YW}<!>{LR}INSTALLING LIBIMOBILEDEVICE WITH GNUTLS..\033[1;33m<!>\033[0m\n"
+printf  "\033[1;33m<!>\033[1;31m MAKING LIBIMOBILEDEVICE USING GNUTLS\033[1;33m<!>\033[0m\n"printf "{YW}<!>{LR}INSTALLING LIBIMOBILEDEVICE WITH GNUTLS..\033[1;33m<!>\033[0m\n"
 
 ./autogen.sh --disable-openssl
 make
 sudo make install
 
+cd ..
+printf  "\033[1;33m<!>\033[1;31m CLONING LIBIDEVICEACTIVATION \033[1;33m<!>\033[0m\n"
+
+git clone https://github.com/libimobiledevice/libideviceactivation
+cd libideviceactivation
+
+printf "\033[1;33m<!> \033[1;31mmaking libideviceactivation\033[1;33m <!>\033[0m\n"
+./autogen.sh
+make
+sudo make install
 
 cd .. 
 printf  "\033[1;33m<!>\033[1;31m CLONING IDEVICERESTORE \033[1;33m<!>\033[0m\n"
@@ -23,7 +37,7 @@ printf  "\033[1;33m<!>\033[1;31m CLONING IDEVICERESTORE \033[1;33m<!>\033[0m\n"
 git clone https://github.com/libimobiledevice/idevicerestore
 cd idevicerestore
 
-printf "\033[1;33m<!> \033[1;31minstalling idevicerestore\033[1;33m <!>{NC}\n"
+printf "\033[1;33m<!> \033[1;31mmaking idevicerestore\033[1;33m <!>\033[0m\n"
 ./autogen.sh
 make
 sudo make install
