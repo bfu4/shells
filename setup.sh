987dfb8270df72d5cfa964fcd5bc0adddfbed682
bfu <46942366+bfu4@users.noreply.github.com>
2020-07-15 03:44:15 -0400

    easy docker setup
    to launch docker using a 'command'

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# prints docker container info
+
+printf "\033[1;31m"
+echo "docker container info: "
+printf "\033[0m"
+docker ps
+# creates new start shell on desktop
+cd ~/Desktop
+
+printf "\033[1;32m"
+echo "what would you like to name your command?"
+
+printf "\033[0m"
+read name
+
+printf "\033[1;33m"
+echo "in the next window, type"
+
+printf "\033[1;36m"
+echo "#/bin/sh"
+echo "docker start (container name)"
+echo "screen -S (title) docker exec -it (container name) /bin/bash"
+echo "docker stop (container name)"
+
+printf "\033[1;39m"
+read -t 20 -p "[Press Enter]"
+printf "\033[0m"
+nano $name
+
+cp $name /usr/local/bin
+
+chmod 777 /usr/local/bin/$name
