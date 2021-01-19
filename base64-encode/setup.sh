799aee915ebb9b9b612ebeb8b76817483aeba643
bfu4 <bella@sleeeepyti.me>
2021-01-18 22:13:49 -0500

    fix setup and gpg test

--- base64-encode/setup.sh
+++ base64-encode/setup.sh
@@ -2,12 +2,12 @@
 
 printf "\033[1;35m[MAKING DIRECTORY]\033[0m\n"
 mkdir /usr/local/share/base64
-cp ToBase64.class /usr/local/share/base64
+cp ToBase64.class /usr/local/share/base64/ToBase64.class
 
 echo
 
-echo "\033[1;35m[INSTALLING SCRIPT]\033[0m\n"
-cp base64 /usr/local/bin
+printf "\033[1;35m[INSTALLING SCRIPT]\033[0m\n"
+cp base64 /usr/local/bin/base64
 echo "done."
 
 read -p "would you like to clear all files? (y/n)" d
@@ -15,14 +15,14 @@ read -p "would you like to clear all files? (y/n)" d
 case $d in
 "y") remove;;
 *) exit;;
-
+esac
 
 remove() {
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd) " 
-cd ..
-rm -rf $DIR/base64
-rm -rf $DIR/ToBase64.class
-rm -rf $DIR/README.md
-rm -rf $DIR/setup.sh
-cd $DIR
+   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd) " 
+   cd ..
+   rm -rf $DIR/base64
+   rm -rf $DIR/ToBase64.class
+   rm -rf $DIR/README.md
+   rm -rf $DIR/setup.sh
+   cd $DIR
 }
